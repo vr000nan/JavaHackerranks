c97e5d40ba450435886b68fcbf39fd3699afcbf0
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2023-09-18 16:41:25 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Here is where I'll be storing my solutions to all of the Java Hackerrank questions!");
+    }
+}
